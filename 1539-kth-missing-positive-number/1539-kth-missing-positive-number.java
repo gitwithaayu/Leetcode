9c6f54adc29173447c7108ad8cea952854ffd756
gitwithaayu <0211aayushi@gmail.com>
2026-07-09 11:23:35 +0530

    Time: 0 ms (100%), Space: 44 MB (98.45%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -2,47 +2,47 @@ class Solution {
     public int findKthPositive(int[] arr, int k) {
        
 
-        ArrayList<Integer> list = new ArrayList<>();
-        int j = 1;
+//         ArrayList<Integer> list = new ArrayList<>();
+//         int j = 1;
 
-        for (int i = 0; i < arr.length; i++) {
+//         for (int i = 0; i < arr.length; i++) {
 
-            if (j != arr[i]) {
-                list.add(j);
-                j++;
-                i--;          // Same array element ko dobara check karo
+//             if (j != arr[i]) {
+//                 list.add(j);
+//                 j++;
+//                 i--;          // Same array element ko dobara check karo
+//             } else {
+//                 j++;
+//             }
+//         }
+
+//         // Array ke baad bhi missing numbers add karo
+//         while (list.size() < k) {
+//             list.add(j);
+//             j++;
+//         }
+
+//         return list.get(k - 1);
+//     }
+// }
+
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            int missing = arr[mid] - (mid + 1);
+
+            if (missing < k) {
+                low = mid + 1;
             } else {
-                j++;
+                high = mid - 1;
             }
         }
 
-        // Array ke baad bhi missing numbers add karo
-        while (list.size() < k) {
-            list.add(j);
-            j++;
-        }
-
-        return list.get(k - 1);
+        return low + k;
     }
 }
-
-
-    //      int low=0;
-    //     int high=arr.length;
-    //     while(low<=high){
-    //     int mid=low+(high-low)/2;
-    //     int missing=arr[mid]-(mid+1);
-    //     if(missing<k){
-    //         low=mid+1;
-    //     }
-    //     else
-    //      high=mid+1;
-    
-    //     }      
-    //      return low+k;   
-
-    //     }
-        
-    // }
-
-       
