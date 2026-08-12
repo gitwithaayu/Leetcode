44f441ccc15fb608d98269c1924f2925d70a1f63
gitwithaayu <0211aayushi@gmail.com>
2026-08-12 21:12:06 +0530

    Time: 0 ms (100%), Space: 44.5 MB (85.42%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,52 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+
+        int pivot = -1;
+
+        // 1. Find pivot
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i] > nums[i - 1]) {
+                pivot = i - 1;
+                break;
+            }
+        }
+
+        // If no pivot, array is the largest permutation
+          if (pivot == -1) {
+            for (int i = 0, j = nums.length - 1; i < j; i++, j--) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+            return;
+        }
+      
+
+        // 2. Find greater element
+        int greater = -1;
+
+        for (int i = nums.length - 1; i > pivot; i--) {
+            if (nums[i] > nums[pivot]) {
+                greater = i;
+                break;
+            }
+        }
+
+        // 3. Swap pivot and greater
+        int temp = nums[pivot];
+        nums[pivot] = nums[greater];
+        nums[greater] = temp;
+
+        // 4. Sort suffix in ascending order
+        for (int i = pivot + 1; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] > nums[j]) {
+                    int t = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = t;
+                }
+            }
+        }
+    }
+}
